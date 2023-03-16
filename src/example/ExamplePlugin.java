c469c369436860170a11084662d6c1a6cd08c45e
RTOmega <62565267+RebornTrack970@users.noreply.github.com>
2023-03-16 19:23:07 +0300

    Update ExamplePlugin.java

--- src/example/ExamplePlugin.java
+++ src/example/ExamplePlugin.java
@@ -2,83 +2,86 @@ package example;
 
 import arc.*;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
 import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
-import mindustry.net.Administration.*;
-import mindustry.world.blocks.storage.*;
+import mindustry.content.*;
+import mindustry.gen.*;
+import mindustry.ui.*;
+import mindustry.ui.dialogs.*;
 
-public class ExamplePlugin extends Plugin{
+public class UnitStrengthMultiplierPlugin extends Plugin {
 
-    //called when game initializes
+    // Store the current strength multipliers for damage and health
+    private float damageMultiplier = 1f;
+    private float healthMultiplier = 1f;
+
+    // Store the time to wait before increasing the multiplier
+    private int timeToWait = 0;
+
+    // Text shown on UI
+    private String uiText = "";
+
     @Override
-    public void init(){
-        //listen for a block selection event
-        Events.on(BuildSelectEvent.class, event -> {
-            if(!event.breaking && event.builder != null && event.builder.buildPlan() != null && event.builder.buildPlan().block == Blocks.thoriumReactor && event.builder.isPlayer()){
-                //player is the unit controller
-                Player player = event.builder.getPlayer();
-
-                //send a message to everyone saying that this player has begun building a reactor
-                Call.sendMessage("[scarlet]ALERT![] " + player.name + " has begun building a reactor at " + event.tile.x + ", " + event.tile.y);
-            }
+    public void init() {
+        // Add a listener for unit spawn events
+        Events.on(UnitSpawnEvent.class, event -> {
+            // Multiply the damage and health of the unit by the current multipliers
+            event.unit.damage *= damageMultiplier;
+            event.unit.maxHealth *= healthMultiplier;
         });
 
-        //add a chat filter that changes the contents of all messages
-        //in this case, all instances of "heck" are censored
-        Vars.netServer.admins.addChatFilter((player, text) -> text.replace("heck", "h*ck"));
+        // Add a listener for the update event
+        Events.on(EventType.Trigger.update, () -> {
+            // Decrease the time to wait by 1
+            timeToWait--;
 
-        //add an action filter for preventing players from doing certain things
-        Vars.netServer.admins.addActionFilter(action -> {
-            //random example: prevent blast compound depositing
-            if(action.type == ActionType.depositItem && action.item == Items.blastCompound && action.tile.block() instanceof CoreBlock){
-                action.player.sendMessage("Example action filter: Prevents players from depositing blast compound into the core.");
-                return false;
+            // If the time to wait is less than or equal to 0
+            if (timeToWait <= 0) {
+                // Increase the damage and health multipliers by 0.1
+                damageMultiplier += 0.1f;
+                healthMultiplier += 0.1f;
+
+                // Reset the time to wait
+                timeToWait = 60;
             }
-            return true;
+
+            // Update the UI text
+            uiText = "[#00FF00]Damage Multiplier: " + String.format("%.1f", damageMultiplier) + "x\n" +
+                     "[#0000FF]Health Multiplier: " + String.format("%.1f", healthMultiplier) + "x";
         });
     }
 
-    //register commands that run on the server
     @Override
-    public void registerServerCommands(CommandHandler handler){
-        handler.register("reactors", "List all thorium reactors in the map.", args -> {
-            for(int x = 0; x < Vars.world.width(); x++){
-                for(int y = 0; y < Vars.world.height(); y++){
-                    //loop through and log all found reactors
-                    //make sure to only log reactor centers
-                    if(Vars.world.tile(x, y).block() == Blocks.thoriumReactor && Vars.world.tile(x, y).isCenter()){
-                        Log.info("Reactor at @, @", x, y);
-                    }
-                }
-            }
+    public void registerServerCommands(CommandHandler handler) {
+        // Add a command that sets the time to wait before increasing the multiplier
+        handler.register("unitConfig", "<time>", "Sets the time to wait before increasing the unit multiplier by 0.1x.", args -> {
+            // Parse the time argument
+            int time = Strings.parseInt(args[0], 0);
+
+            // Set the time to wait
+            timeToWait = time;
+
+            // Show a message to the admin
+            Call.infoPopup("Time to wait set to " + timeToWait + " seconds.", 5f);
         });
     }
 
-    //register commands that player can invoke in-game
     @Override
-    public void registerClientCommands(CommandHandler handler){
-
-        //register a simple reply command
-        handler.<Player>register("reply", "<text...>", "A simple ping command that echoes a player's text.", (args, player) -> {
-            player.sendMessage("You said: [accent] " + args[0]);
-        });
-
-        //register a whisper command which can be used to send other players messages
-        handler.<Player>register("whisper", "<player> <text...>", "Whisper text to another player.", (args, player) -> {
-            //find player by name
-            Player other = Groups.player.find(p -> p.name.equalsIgnoreCase(args[0]));
-
-            //give error message with scarlet-colored text if player isn't found
-            if(other == null){
-                player.sendMessage("[scarlet]No player by that name found!");
-                return;
-            }
-
-            //send the other player a message, using [lightgray] for gray text color and [] to reset color
-            other.sendMessage("[lightgray](whisper) " + player.name + ":[] " + args[1]);
+    public void registerClientCommands(CommandHandler handler) {
+        // Add a command that shows the current strength multipliers
+        handler.<Player>register("unitConfig", "Shows the current unit strength multipliers.", (args, player) -> {
+            // Show the UI text to the player
+            Call.onInfoToast(player.con, uiText, 10f);
         });
     }
+
+    @Override
+    public void registerClientModElements() {
+        // Add a UI element to show the current strength multipliers
+        BaseDialog dialog = new BaseDialog("Unit Multipliers");
+        dialog.cont.add(new Label(() -> uiText)).grow().wrap().pad(10f);
+        dialog.addCloseButton();
+        Core.scene.dialog.addDialog(dialog);
+    }
+
 }
