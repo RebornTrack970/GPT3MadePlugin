d2714dbbae7a28c929d0c93abb8f9dae71aef84a
RTOmega <62565267+RebornTrack970@users.noreply.github.com>
2023-03-16 19:39:17 +0300

    Update UnitStrengthMultiplierPlugin.java

--- src/example/UnitStrengthMultiplierPlugin.java
+++ src/example/UnitStrengthMultiplierPlugin.java
@@ -1,47 +1,74 @@
 package example;
 
-import arc.*;
-import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
-import mindustry.game.EventType.*;
-import mindustry.gen.*;
-import mindustry.mod.*;
+import arc.Events;
+import arc.math.Mathf;
+import arc.util.Time;
+import mindustry.game.EventType;
+import mindustry.game.Team;
+import mindustry.gen.Call;
+import mindustry.gen.Groups;
+import mindustry.gen.Player;
+import mindustry.gen.Unit;
+import mindustry.mod.Plugin;
 
 public class UnitStrengthMultiplierPlugin extends Plugin {
-    private float damageMultiplier = 1f;
-    private float healthMultiplier = 1f;
-    private float timeToWait = 600f;
-    private String uiText;
 
+    private float damageMultiplier = 2.0f;
+    private float timeToWait = 10.0f;
+
+    @Override
     public void registerClientCommands(CommandHandler handler){
-        handler.<Player>register("unitConfig", "<timeInSecondToWaitBeforeIncreaseBy0.1x>", "Set the time to wait before increasing unit damage and health by 0.1x", (args, player) -> {
-            timeToWait = Float.parseFloat(args[0]);
-            Call.infoPopup("Time to wait set to " + timeToWait + " seconds.", 5f, player);
-        });
-    }
+        handler.register("multiply-damage", "Sets the damage multiplier for units.", (args) -> {
+            if(args.length == 0){
+                Call.infoPopup("Usage: /multiply-damage [multiplier]", 5f, null);
+                return;
+            }
 
-    public void init(){
-        uiText = "DM: [#" + "9b383b" + "]" + String.format("%.1f", damageMultiplier) + "[] HM: [#" + "3a74c5" + "]" + String.format("%.1f", healthMultiplier) + "[]";
-        Events.on(UnitSpawnEvent.class, event -> {
-            Unit unit = event.unit();
-            unit.maxHealth(unit.maxHealth() * healthMultiplier);
-            unit.health(unit.health() * healthMultiplier);
-            unit.damage(unit.damage() * damageMultiplier);
-        });
-
-        Events.on(EventType.Trigger.update, () -> {
-            if(Time.time() % timeToWait < 0.05f){
-                damageMultiplier += 0.1f;
-                healthMultiplier += 0.1f;
-                uiText = "DM: [#" + "9b383b" + "]" + String.format("%.1f", damageMultiplier) + "[] HM: [#" + "3a74c5" + "]" + String.format("%.1f", healthMultiplier) + "[]";
+            try {
+                float newMultiplier = Float.parseFloat(args[0]);
+                damageMultiplier = newMultiplier;
+                Call.infoPopup("Damage multiplier set to " + damageMultiplier, 5f, null);
+            } catch (NumberFormatException e){
+                Call.infoPopup("Invalid multiplier value.", 5f, null);
             }
         });
     }
 
+    @Override
     public void registerServerCommands(CommandHandler handler){
+        handler.register("multiply-damage", "Sets the damage multiplier for units.", (args) -> {
+            if(args.length == 0){
+                Log.info("Usage: /multiply-damage [multiplier]");
+                return;
+            }
+
+            try {
+                float newMultiplier = Float.parseFloat(args[0]);
+                damageMultiplier = newMultiplier;
+                Log.info("Damage multiplier set to " + damageMultiplier);
+            } catch (NumberFormatException e){
+                Log.info("Invalid multiplier value.");
+            }
+        });
     }
 
-    public UnitStrengthMultiplierPlugin(){
+    @Override
+    public void init(){
+        Events.on(UnitSpawnEvent.class, event -> {
+            Unit unit = event.unit;
+            if(unit.team() != Team.derelict && unit.team() != Team.crux){
+                unit.damage(unit.health() * damageMultiplier);
+            }
+        });
+
+        Events.on(EventType.Trigger.update, () -> {
+            if(Mathf.mod(Time.time(), timeToWait) < 0.05f){
+                Groups.unit.each(unit -> {
+                    if(unit.team() != Team.derelict && unit.team() != Team.crux){
+                        unit.damage(unit.health() * (damageMultiplier - 1));
+                    }
+                });
+            }
+        });
     }
 }
